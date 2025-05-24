19f3189f7a40a24e44658c41cb899d089ec937f9
Yaz <gamersensey847@gmail.com>
2025-05-24 10:31:26 +0100

    Update apiConfig.js

--- configs/apiConfig.js
+++ configs/apiConfig.js
@@ -1,7 +1,7 @@
 const apiConfig = {
   MONGODB_URI: "mongodb+srv://Wudysoft:wudysoft@wudysoft.2hm26ic.mongodb.net/Api?retryWrites=true&w=majority&appName=Wudysoft",
   JWT_SECRET: "JWT_SECRET",
-  DOMAIN_URL: "wudysoft.xyz",
+  DOMAIN_URL: "wudyver-d132.onrender.com",
   LIMIT_POINTS: 5,
   LIMIT_DURATION: 30
 };
